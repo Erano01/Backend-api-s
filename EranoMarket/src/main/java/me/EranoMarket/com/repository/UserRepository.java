98d8f3b95562fc984208f3b4afde857860b3d439
Ali <63211639+Serterano@users.noreply.github.com>
2023-06-04 03:53:11 +0300

    Delete EranoMarket directory

--- EranoMarket/src/main/java/me/EranoMarket/com/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package me.EranoMarket.com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import me.EranoMarket.com.model.User;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
